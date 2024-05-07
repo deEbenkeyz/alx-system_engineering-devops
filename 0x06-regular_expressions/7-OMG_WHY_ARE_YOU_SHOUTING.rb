815439b2c4644c88ff17677cbf5d77c263c54017
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2024-05-07 16:34:24 +0000

    Checking for capital letters

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby                                                                                                                                                       
+#This is project was done by Ebenezer Ohene-Adutwum                                                                                                                        
+puts ARGV[0].scan(/[A-Z]*/).join
