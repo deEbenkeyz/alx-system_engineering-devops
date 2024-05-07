5e373311e204c50b8840628ae92b9e996ffc76b9
Ebenezer Ohene-Adutwum <eben.oadutwum@gmail.com>
2024-05-07 16:19:17 +0000

    Task 3 completed

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby                                                                                                                                                       
+#This is project was done by Ebenezer Ohene-Adutwum                                                                                                                        
+puts ARGV[0].scan(/hbt{1,4}n/).join
